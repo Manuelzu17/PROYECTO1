f97da8ea6f6b0c623bb94ce4633508919fce1545
Manuelzu17 <manuel3783822@gmail.com>
2022-03-14 17:28:40 -0500

    martes14

---
+++ python/bucless.py
@@ -0,0 +1,7 @@
+for letra in "python":
+    
+    if letra=="h":
+        continue
+    
+    print("viendo la letra:" + letra)
+       
---
+++ python/generadores.py
@@ -0,0 +1,27 @@
+from operator import ne
+
+
+def generaPares(limite):
+    
+    num=1
+    
+    miLista=[]
+    
+    while num<limite:
+        
+        miLista.append(num*2)
+        
+        num=1+num
+    
+    return miLista
+
+print(generaPares(10))
+
+def devuelve_ciudades(*ciudades):
+    for elemento in ciudades:
+        yield elemento
+        
+devuelve_ciudades=devuelve_ciudades("Madrid","Barcelona","Bilbao","Valencia")
+
+print(next(devuelve_ciudades))
+print(next(devuelve_ciudades))
